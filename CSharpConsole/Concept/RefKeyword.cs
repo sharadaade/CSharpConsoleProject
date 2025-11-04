9905d775a4b66902323d2d4ebbd679fb740d9389
sharadaade <aadesharad@email.com>
2025-11-05 02:02:09 +0530

    out keyword

---
+++ CSharpConsole/Concept/RefKeyword.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole.Concept
+{
+    internal class RefKeyword
+    {
+        //With ref
+        static void Increase(ref int number)
+        {
+            number = number + 10;
+        }
+
+        // Without ref
+        static void Increase(int num)
+        {
+            num = num + 10;
+        }
+        static void Main()
+        {
+            //With ref call 
+            int value = 5;
+            Increase(ref value);
+            Console.WriteLine(value);
+
+
+            // Without ref call
+            int val = 5;
+            Increase(value);
+            Console.WriteLine(value); // Output: 5
+        }
+
+        
+
+        
+    }
+}
