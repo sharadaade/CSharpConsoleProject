954d34e1f17038150a66b649def8c24d80fb9a0b
sharadaade <aadesharad@email.com>
2025-11-24 23:48:18 +0530

    Reverse each word in statement

---
+++ CSharpConsole/Lab/ReverseWordStatement.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharpConsole.Lab
+{
+    internal class ReverseWordStatement
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World from C#";
+            string result = "";
+            string currentWord = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+
+                if (ch != ' ')
+                {
+                    // Reverse by prepending
+                    currentWord = ch + currentWord;
+                }
+                else
+                {
+                    // Add reversed word to result
+                    result += currentWord + " ";
+                    currentWord = "";
+                }
+            }
+
+            // Add last reversed word
+            result += currentWord;
+
+        }
+    }
+}
