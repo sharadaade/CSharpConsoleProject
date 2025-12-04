c89f35b57c231a037ce3a932a108e511fdef541f
sharadaade <aadesharad@email.com>
2025-12-05 02:29:11 +0530

    collections started

---
+++ CSharpConsole/CollectionsAll/CollectionsOne.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole.CollectionsAll
+{
+    internal class CollectionsOne
+    {
+        static void Main(string[] args)
+        {
+            char c = 'M';
+            Console.WriteLine(sizeof(char));
+            // ==== Static size ==============
+            int[] ar = new int[10];
+            //Console.WriteLine(ar.Length);
+
+            // Old item is cpoied from old array and new array is created
+            // Later on old array is destroyed by GC(Garbage collector)
+            Array.Resize(ref ar, 20);
+            //Console.WriteLine(ar.Length);
+
+            // ============ Variable length ==============
+            // Auto Resize => 0, 4, 8, 16, 32, 64, 128, ...
+            ArrayList al = new ArrayList();
+            Console.WriteLine(al.Capacity);
+            al.Add(100);
+            al.Add(200);
+            al.Add(300);
+            al.Add(400);
+            al.Add(150);
+            al.Add(500);
+            al.Add(600);
+
+            Console.WriteLine(al.Capacity);
+
+            foreach (object item in al)
+            {
+                // String Interpolation
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+
+            // Insert item at specific index
+            al.Insert(1, 150);
+
+            // NOTE => Remove the first occurence of element from ArrayList
+            // Rmove element which you want pass as parameter
+            al.Remove(150);
+            
+            // Remove element from specific index
+            //al.RemoveAt(6);
+            foreach(object item in al)
+            {
+                // String Interpolation
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
---
+++ CSharpConsole/CollectionsAll/HashTableDemo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole.CollectionsAll
+{
+    internal class HashTableDemo
+    {
+        static void Main(string[] args)
+        {
+            // Before going to Hashtable disscuss the problem with Array and ArrayList
+            // Array and ArrayList have predefinde key measns index(0, 1, 2, 3, 4, ....)
+            // We can't change 
+            // But storing the User details is ok but accessing the details is very problematic
+            // We need to remember 0 for Id, 1 for Name, 2 for Address, 3 for Job and so on...
+
+            // This problem is solved by Hashtable
+            // Array and ArrayList is we can use for other purpose 
+
+            // Let's go => Hashtable
+
+            Hashtable ht = new Hashtable();
+            ht.Add("Id", 1001);
+            ht.Add("Name", "Sharad");
+            ht.Add("Job", ".NET Developer");
+            ht.Add("Salary", 250000);
+            ht.Add("Phone", 7218002500);
+            ht.Add("Email", "aadesharad@gmail.com");
+            ht.Add("Location", "MH");
+
+            //Console.WriteLine(ht["Name"]);
+            //Console.WriteLine(ht["Job"]);
+            //Console.WriteLine(ht["Email"]);
+
+            // Get all the keys    ****
+            foreach(object x in ht.Keys)
+            {
+                Console.WriteLine(x);
+            }
+
+            // Note => It not shows in sequential order as we store
+            // Because internal Hashtable create hashcode(int) for every key's based on that it shows
+            // Hastable contains three things => 1.Key 2.Value 3.Hashcode
+
+
+            Console.WriteLine();
+            
+
+
+            // Sample to check the hashcode
+            //Console.WriteLine("Hello".GetHashCode());
+
+        }
+    }
+}
