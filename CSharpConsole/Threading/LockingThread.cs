b3b5a28c03c0bc04f1ccf402cb6c010f12aa7148
sharadaade <aadesharad@email.com>
2025-12-04 19:43:12 +0530

    thread-locking

---
+++ CSharpConsole/Threading/LockingThread.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CSharpConsole.Threading
+{
+    internal class LockingThread
+    {
+        public void Display()
+        {
+            // lock is allow only one thread can access code at a time
+            // Other thread will wait untile execution completion
+            lock(this)
+            {
+                Console.Write("C# is an - ");
+                Thread.Sleep(5000);
+                Console.WriteLine("Object oriented programming language.");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            LockingThread obj = new LockingThread();
+
+            Thread t1 = new Thread(obj.Display);
+            Thread t2 = new Thread(obj.Display);
+            Thread t3 = new Thread(obj.Display);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
---
+++ CSharpConsole/Threading/ThreadingDemo3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CSharpConsole.Threading
+{
+    internal class ThreadingDemo3
+    {
+        //static void Test()
+        //{
+        //    for(int i = 0; i < 100; i++)
+        //    {
+        //        Console.WriteLine("Test " + i);
+        //    }
+        //}
+
+        static void Test(object max)
+        {
+            int num = Convert.ToInt32(max);
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Test " + i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //ThreadStart ts = new ThreadStart(Test);
+            //Thread th = new Thread(Test);
+            //th.Start();
+
+            //ThreadStart obj = () => Test();
+            //Thread t = new Thread(obj);
+            //t.Start();
+
+            ParameterizedThreadStart tsObj = new ParameterizedThreadStart(Test);
+            Thread t = new Thread(tsObj);
+            t.Start("Hello");
+
+        }
+    }
+}
