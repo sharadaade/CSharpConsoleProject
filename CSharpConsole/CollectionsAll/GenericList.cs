dc4b5ad9dd5d372b156c612c77958a0725ea207d
sharadaade <aadesharad@email.com>
2025-12-06 01:24:35 +0530

    generic List<T> added

---
+++ CSharpConsole/CollectionsAll/GenericList.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+namespace CSharpConsole.CollectionsAll
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string Name { set; get; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+
+    internal class GenericList
+    {
+        static void Main(string[] args)
+        {
+            // ===================== List ==========================
+            // List is generic collection which is used to store similar type of data
+            // List are present in System.Collections.Generic
+            // 
+            List<int> l = new List<int>();
+
+            List<string> s = new List<string>();
+
+            List<Person> p = new List<Person>();
+
+            l.Add(1);
+            l.Add(22);
+            l.Add(43);
+            l.Add(4);
+            //l.Add(52);
+            l.Remove(11);
+            l.Sort();
+            var newArr = l.ToArray();
+            Array.Sort(newArr);
+            foreach(var i in newArr)
+            {
+                Console.Write($"{i} ");
+            }
+
+            //Console.WriteLine(l.Sum());
+            //foreach(int item in l)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.Write(l.Capacity);
+
+            s.Add("Mango");
+            s.Add("Banana");
+            s.Add("Pineapple");
+            //Console.WriteLine(s.Capacity);
+        }
+    }
+}
---
+++ CSharpConsole/CollectionsAll/QueueDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole.CollectionsAll
+{
+    internal class QueueDemo
+    {
+        static void Main(string[] args)
+        {
+            // ================= Queue ==================
+            // 
+            Queue q = new Queue(10);
+            Console.WriteLine(q.Count);
+            q.Enqueue(10);
+            q.Enqueue(20);
+            q.Enqueue(30);
+            q.Enqueue(40);
+            Console.WriteLine(q.Dequeue());
+            Console.WriteLine(q.Count);
+        }
+    }
+}
