838898675650bf5b27de2e1261cc6d92cbefc27f
sharadaade <aadesharad@email.com>
2025-12-05 23:01:42 +0530

    stack concept added

---
+++ CSharpConsole/CollectionsAll/ArrayListDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole.CollectionsAll
+{
+    internal class ArrayListDemo
+    {
+        static void Main(string[] args)
+        {
+            // ===========  ArrayList =============
+            // 
+            ArrayList a = new ArrayList(20);
+            a.Add(100);
+            a.Add(200);
+            a.Add(300);
+            a.Add(400);
+            a.Add(500);
+            a.Add(100);
+            a.Add(200);
+            a.Add(300);
+            a.Add(400);
+            a.Add(500);
+            a.Add(100);
+            a.Add(200);
+            a.Add(300);
+            a.Add(400);
+            a.Add(500);
+            a.Add(100);
+            a.Add(200);
+            a.Add(300);
+            a.Add(400);
+            a.Add(500);
+            a.Add(900);
+            Console.WriteLine(a.Capacity);
+        }
+    }
+}
---
+++ CSharpConsole/CollectionsAll/StackDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole.CollectionsAll
+{
+    internal class StackDemo
+    {
+        static void Main(string[] args)
+        {
+            // =========== Stack ============
+            // Stack works
+            // FILO => First In Last Out
+            // LIFI => Last In First Out
+
+            //============================================
+            // Common methods => .Push(), .Pop(), .Peek()
+            // Common property => .Count
+            //============================================
+            Stack st = new Stack(6);
+            //Console.WriteLine(st.Count);
+
+            // Push() => It Add the object at the top in stack
+
+            st.Push(100);
+            st.Push(200);
+            st.Push(300);
+            st.Push(400);
+            st.Push(500);
+            st.Push(600);
+            st.Push(700);
+
+            // Count => property
+            // Count property gets the number of elemetn contains in stack
+            //Console.WriteLine(st.Count);
+
+            // Pop() method removes the top most object from stack
+            //st.Pop();
+
+            foreach (object i in st)
+            {
+                Console.Write($"{i} ");
+            }
+
+            // Peek() => It return Top most object from stack without removing
+            //Console.WriteLine(st.Peek());
+
+            
+        }
+    }
+}
