c6a202a0d49514e504b9535d92035be43d7ac2e0
sharadaade <aadesharad@email.com>
2026-01-30 18:34:48 +0530

    TryParse example

---
+++ CSharpConsole/Concept/TryParseExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole.Concept
+{
+    internal class TryParseExample
+    {
+        static void Main(string[] args)
+        {
+            bool success = int.TryParse("123", out int result);
+            Console.WriteLine(success);
+            Console.WriteLine(result.GetType());
+
+        }
+    }
+}
